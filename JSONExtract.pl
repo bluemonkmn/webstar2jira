b9bb95c359edf2094c46a7f760f369779ca5f94e
unknown <bmarty@USSPDBMARTY.infor.com>
2014-06-25 16:12:29 -0500

    Use indented JSON for easier reading and editing during development. Replace “externalId” with “key” to see if we can use the standard “key” field to track our SDR numbers. Include “SDR-” prefix on the key values. Specified “issueType”: “Bug”. Duplicate some comments as history entries. Try mapping status codes to standard/default values, but using our non-standard strings, and without specifying “original” values: a. “Reported” = 1, “Reported” b. “Wait” = 3, “Wait” c. “Closed” = 3, “Closed” d. “Open” = 4, “Open” Try mapping reason codes to extra fields (targeting “resolution” field) in the same history entries without “original” values *and* without codes, but *using* standard JIRA resolution strings: a. “Fixed” => “Fixed” b. “Duplicate” => “Duplicate” c. “No Fix” => “Won’t Fix” d. “No Problem”; “Mystery” => “Cannot Reproduce” e. “Need More Info” => “Incomplete”

--- JSONExtract.pl
+++ JSONExtract.pl
@@ -1,6 +1,9 @@
 use DBI;
 use JSON;
 use Data::Printer;
+use 5.10.1;
+
+no if $] >= 5.018, warnings => "experimental";
 
 my $dbs = 'dbi:ODBC:DRIVER={SQL Server};SERVER=.\R2;Integrated Security=Yes';
 my $dbh = DBI->connect($dbs);
@@ -13,7 +16,7 @@ if (defined($dbh))
 	$dbh->{LongReadLen} = 50000;
 	
 	my $query = <<'~';
-	select SDRNum externalId
+	select 'SDR-' + cast(SDRNum as varchar(50)) [key]
 	, case Severity when 'A' then 'P1' when 'B' then 'P2' when 'C' then 'P3' when 'D' then 'P4' else null end priority
 	, case Status when 'W' then 'Awaiting Approval' when 'C' then 'Resolved' else 'Accepted' end status
 	, ReasonCode resolution
@@ -24,6 +27,7 @@ if (defined($dbh))
 	, case when s.Level3 is null then s.Level2 else s.Level2 + '_' + s.Level3 end components
 	, ProblemBrief summary
 	, REPLACE(cast(ProblemDetail as varchar(max)), CHAR(13) + CHAR(10), CHAR(10)) description
+	, 'Bug' issueType
 	from STAR..sdr s
 	left join STAR..UserInfo u on u.UserName = s.AssignedTo
 	left join STAR..UserInfo us on us.UserName = s.Submitter
@@ -32,9 +36,9 @@ if (defined($dbh))
 	#, convert(varchar(20), s.DateClosed, 126) resolved
 
 	my $sth = $dbh->prepare($query);
-	$sth->execute();
+	$sth->execute() or die 'Failed to execute SDR query.';
 
-	my $json = JSON->new->allow_nonref;
+	my $json = JSON->new->allow_nonref->pretty;
 	my @sdrArray;
 	my %sdrLookup = ();
 	my %component_list = ();
@@ -47,15 +51,17 @@ if (defined($dbh))
 		$version_list{$hashref->{'affectedVersions'}} = 1;
 		$hashref->{'affectedVersions'} = [$hashref->{'affectedVersions'}];
 		push @sdrArray, $hashref;
-		$sdrLookup{$hashref->{'externalId'}} = $hashref;
+		$sdrLookup{$hashref->{'key'}} = $hashref;
 	}
 	
 	$sth->finish;
 
 	$query = <<'~';
-	select SDR_Num externalId
+	select 'SDR-' + cast(SDR_Num as varchar(50)) [issueKey]
 	,convert(varchar(25), EntryDate, 126) created
 	,isnull(p.NTUserName, null) author
+	,LineType
+	,ReasonCode
 	,LineType + case when p.NTUserName is null then ' (' + l.Person + '): ' else ': ' end + isnull(cast([Description] as varchar(max)),'') + ISNULL(ReasonCode, '') body
 	from STAR..sdr_log l
 	left join STAR..UserInfo p on l.Person = p.UserName
@@ -66,10 +72,10 @@ if (defined($dbh))
 	
 	while (my $comments = $sth->fetchrow_hashref())
 	{
-		my $sdr = $sdrLookup{$comments->{'externalId'}};
+		my $sdr = $sdrLookup{$comments->{'issueKey'}};
 		next if (!$sdr);
-		# Remove the externalId column from the object before encoding it into JSON
-		delete $comments->{'externalId'};		
+		# Remove the issueKey column from the object before encoding it into JSON
+		delete $comments->{'issueKey'};		
 		if ($sdr->{'comments'})
 		{
 			push $sdr->{'comments'}, $comments;
@@ -78,6 +84,55 @@ if (defined($dbh))
 		{
 			$sdr->{'comments'} = [$comments];
 		}
+
+		my $histTo;
+		my $reason;
+		given (lc $comments->{LineType})
+		{
+			when ('reported') {$histTo = 1;}
+			when ('wait') {$histTo = 3;}
+			when ('closed') {$histTo = 3;}
+			when ('open') {$histTo = 4;}
+			default {undef $histTo;}
+		}
+		given (lc $comments->{ReasonCode})
+		{
+			when ('fixed') {$reason = 'Fixed';}
+			when ('duplicate') {$reason = 'Duplicate';}			
+			when ('no fix') {$reason = 'Won\'t Fix';}
+			when (['no problem', 'mystery']) {$reason = 'Cannot Reproduce';}
+			when ('need more info') {$reason = 'Incomplete';}
+			default { undef $reason; }
+		}
+		if ($histTo)
+		{		
+			my $history = {
+				author=>$comments->{'author'},
+				created=>$comments->{'created'},
+				items=> [
+					{
+					fieldType=>'jira',
+					field=>'status',
+					to=>$histTo,
+					toString=>$comments->{LineType}
+					}
+				]
+			};
+			if ($reason)
+			{
+				push $history->{'items'}, {fieldType=>'jira',field=>'resolution',toString=>$reason};
+			}
+			if ($sdr->{'history'})
+			{
+				push $sdr->{'history'}, $history;
+			}
+			else
+			{
+				$sdr->{'history'} = [$history];				
+			}
+		}
+		delete $comments->{'LineType'};
+		delete $comments->{'ReasonCode'};		
 	}
 	my %import = (projects => [{name=>'JSON Importer Test', key=>'JIT',
 		components=>[keys %component_list], versions=>[map({name=>$_}, keys %version_list)], issues=>\@sdrArray}]);
