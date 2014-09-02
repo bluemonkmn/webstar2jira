608d2807dc5a8a2f4aaeafee60151d0742d3bff4
unknown <bmarty@USSPDBMARTY.infor.com>
2014-09-02 08:10:17 -0500

    Update stream naming code to work when targeting the root stream. Wait for manual retrieval of code from SourceSafe because command line doesn't retrieve all files like VSS UI does.

--- Migrate.pl
+++ Migrate.pl
@@ -5,159 +5,165 @@ my $accurev = 'c:\Program Files (x86)\AccuRev\bin\accurev.exe';
 my $sscmd = 'c:\Program Files (x86)\Microsoft Visual SourceSafe\ss.exe';
 my $tmp = $ENV{'TMP'};
 my $depot = 'FS';
-my $rootStream = ''; # If depot is 'FS', rootStream is 'Auto' and prepend is 'Ben', all streams will go under FS_BenAuto.
 my $prepend = ''; # prepended to created stream names in case multiple test imports need to be done.
+my $rootStream = $prepend ? "${depot}_${prepend}" : $depot;
 my $ssdb73 = 'C:\Users\bmarty\Downloads\R73';
 my $ssdb74 = 'C:\Users\bmarty\Downloads\R74';
 my $ssdb75 = 'C:\Users\bmarty\Downloads\R75';
 my $wsDir = "C:\\Users\\bmarty\\AccuRev\\${depot}_${prepend}Migrate";
-my $ss73 = '$/R73pin/';
-my $ss74 = '$/R74pin/';
-my $ss75 = '$/R75/';
+my $ss73 = '$/R73pin';
+my $ss74 = '$/R74pin';
+my $ss75 = '$/R75';
 
 LogMsg('Starting migration at ' . localtime());
 
 # 7.30
 $ENV{'SSDIR'}=$ssdb73;
-MakeWorkspace('Migrate', $rootStream, $wsDir);
+MakeWorkspace(StmName('Migrate'), $rootStream, $wsDir);
 VSSWorkFold($ss73, $wsDir);
 chdir $wsDir or die $!;
 VSSGet($ss73,'7.30');
 CommitAll('Import FS Label 7.30');
-MakeSnapshot('7.30_GA', $rootStream);
-MakeStream('7.30', '7.30_GA');
-ReparentWorkspace('Migrate', '7.30');
+MakeSnapshot(StmName('7.30_GA'), $rootStream);
+MakeStream(StmName('7.30'), StmName('7.30_GA'));
+ReparentWorkspace(StmName('Migrate'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73,'7.30A');
 CommitAll('Import FS Label 7.30A');
-MakeSnapshot('7.30A_SP', '7.30');
+MakeSnapshot(StmName('7.30A_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30B');
 CommitAll('Import FS Label 7.30B');
-MakeSnapshot('7.30B_SP', '7.30');
+MakeSnapshot(StmName('7.30B_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30C');
 CommitAll('Import FS Label 7.30C');
-MakeSnapshot('7.30C_SP', '7.30');
+MakeSnapshot(StmName('7.30C_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30D');
 CommitAll('Import FS Label 7.30D');
-MakeSnapshot('7.30D_SP', '7.30');
+MakeSnapshot(StmName('7.30D_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30E');
 CommitAll('Import FS Label 7.30E');
-MakeSnapshot('7.30E_SP', '7.30');
+MakeSnapshot(StmName('7.30E_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30F');
 CommitAll('Import FS Label 7.30F');
-MakeSnapshot('7.30F_SP', '7.30');
+MakeSnapshot(StmName('7.30F_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30G');
 CommitAll('Import FS Label 7.30G');
-MakeSnapshot('7.30G_SP', '7.30');
+MakeSnapshot(StmName('7.30G_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30H');
 CommitAll('Import FS Label 7.30H');
-MakeSnapshot('7.30H_SP', '7.30');
+MakeSnapshot(StmName('7.30H_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30I');
 CommitAll('Import FS Label 7.30I');
-MakeSnapshot('7.30I_SP', '7.30');
+MakeSnapshot(StmName('7.30I_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30J');
 CommitAll('Import FS Label 7.30J');
-MakeSnapshot('7.30J_SP', '7.30');
+MakeSnapshot(StmName('7.30J_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, '7.30K');
 CommitAll('Import FS Label 7.30K');
-MakeSnapshot('7.30K_SP', '7.30');
+MakeSnapshot(StmName('7.30K_SP'), StmName('7.30'));
 RecursiveDelete();
 VSSGet($ss73, 'R7.30L');
 CommitAll('Import FS Label R7.30L');
-MakeSnapshot('7.30L_SP', '7.30');
-MakeStream('7.30L', '7.30L_SP');
-MakeStream('7.30_WorkComplete', '7.30L');
-ReparentWorkspace('Migrate', '7.30_WorkComplete');
+MakeSnapshot(StmName('7.30L_SP'), StmName('7.30'));
+MakeStream(StmName('7.30L'), StmName('7.30L_SP'));
+MakeStream(StmName('7.30_WorkComplete'), StmName('7.30L'));
+ReparentWorkspace(StmName('Migrate'), StmName('7.30_WorkComplete'));
 RecursiveDelete();
 VSSGetLatest($ss73);
 CommitAll('Import latest FS 7.30 code after 7.30L release.');
  # 7.40
-ReparentWorkspace('Migrate', $rootStream);
+ReparentWorkspace(StmName('Migrate'), $rootStream);
 $ENV{'SSDIR'}=$ssdb74;
 VSSWorkFold($ss74, $wsDir);
 RecursiveDelete();
 VSSGet($ss74, '7.40');
 CommitAll('Import FS Label 7.40');
-MakeSnapshot('7.40_GA', $rootStream);
-MakeStream('7.40', '7.40_GA');
-ReparentWorkspace('Migrate', '7.40');
+MakeSnapshot(StmName('7.40_GA'), $rootStream);
+MakeStream(StmName('7.40'), StmName('7.40_GA'));
+ReparentWorkspace(StmName('Migrate'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40A');
 CommitAll('Import FS Label 7.40A');
-MakeSnapshot('7.40A_SP', '7.40');
+MakeSnapshot(StmName('7.40A_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40B');
 CommitAll('Import FS Label 7.40B');
-MakeSnapshot('7.40B_SP', '7.40');
+MakeSnapshot(StmName('7.40B_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40C');
 CommitAll('Import FS Label 7.40C');
-MakeSnapshot('7.40C_SP', '7.40');
+MakeSnapshot(StmName('7.40C_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40D');
 CommitAll('Import FS Label 7.40D');
-MakeSnapshot('7.40D_SP', '7.40');
+MakeSnapshot(StmName('7.40D_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40E');
 CommitAll('Import FS Label 7.40E');
-MakeSnapshot('7.40E_SP', '7.40');
+MakeSnapshot(StmName('7.40E_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40F');
 CommitAll('Import FS Label 7.40F');
-MakeSnapshot('7.40F_SP', '7.40');
+MakeSnapshot(StmName('7.40F_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40G');
 CommitAll('Import FS Label 7.40G');
-MakeSnapshot('7.40G_SP', '7.40');
+MakeSnapshot(StmName('7.40G_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40H');
 CommitAll('Import FS Label 7.40H');
-MakeSnapshot('7.40H_SP', '7.40');
+MakeSnapshot(StmName('7.40H_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40I');
 CommitAll('Import FS Label 7.40I');
-MakeSnapshot('7.40I_SP', '7.40');
+MakeSnapshot(StmName('7.40I_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40J');
 CommitAll('Import FS Label 7.40J');
-MakeSnapshot('7.40J_SP', '7.40');
+MakeSnapshot(StmName('7.40J_SP'), StmName('7.40'));
 RecursiveDelete();
 VSSGet($ss74, '7.40K');
 CommitAll('Import FS Label 7.40K');
-MakeSnapshot('7.40K_SP', '7.40');
-MakeStream('7.40K', '7.40K_SP');
-MakeStream('7.40_WorkComplete', '7.40K');
-ReparentWorkspace('Migrate', '7.40_WorkComplete');
+MakeSnapshot(StmName('7.40K_SP'), StmName('7.40'));
+MakeStream(StmName('7.40K'), StmName('7.40K_SP'));
+MakeStream(StmName('7.40_WorkComplete'), StmName('7.40K'));
+ReparentWorkspace(StmName('Migrate'), StmName('7.40_WorkComplete'));
 RecursiveDelete();
 VSSGetLatest($ss74);
 CommitAll('Import latest FS 7.40 code after 7.40K release.');
 # 7.50
-MakeStream('7.50', $rootStream);
-ReparentWorkspace('Migrate', '7.50', $wsDir);
+MakeStream(StmName('7.50'), $rootStream);
+ReparentWorkspace(StmName('Migrate'), StmName('7.50'));
 $ENV{'SSDIR'}=$ssdb75;
 VSSWorkFold($ss75, $wsDir);
 RecursiveDelete();
 VSSGetByDate($ss75, '7-23-2006');
 CommitAll('Import initial FS 7.50 source tree state as of 7-23-2006.');
 
+sub StmName {
+	return "${depot}_${prepend}" . $_[0];
+}
+
 sub VSSGet {
 	my $ssPath = $_[0];
 	my $label = $_[1];
-	system("\"$sscmd\" Get \"$ssPath\" -R -GF -GWR -I-Y -W -Vl$label");
-	if ($?)
-	{
-		die "Failed to retrieve $label";
-	}
+	print "From $ssPath retrieve label $label, then press enter to continue.\n";
+	<STDIN>;
+	#system("\"$sscmd\" Get \"$ssPath\" -R -GF -GWR -I-Y -W -Vl$label");
+	#if ($?)
+	#{
+	#	die "Failed to retrieve $label";
+	#}
 	system('del *.scc /S /Q /F');
 	if ($?)
 	{
@@ -192,11 +198,13 @@ sub VSSGetByDate {
 
 sub VSSGetLatest {
 	my $ssPath = $_[0];
-	system("\"$sscmd\" Get \"$ssPath\" -R -GF -GWR -I-Y -W");
-	if ($?)
-	{
-		die "Failed to retrieve latest code.";
-	}
+	print "From $ssPath retrieve the latest code, then press enter to continue.\n";
+	<STDIN>;
+	#system("\"$sscmd\" Get \"$ssPath\" -R -GF -GWR -I-Y -W");
+	#if ($?)
+	#{
+	#	die "Failed to retrieve latest code.";
+	#}
 	system('del *.scc /S /Q /F');
 	if ($?)
 	{
@@ -235,20 +243,20 @@ sub CommitAll {
 }
 
 sub MakeStream {
-	my $newStream = "${depot}_${prepend}" . $_[0];
-	my $basis = "${depot}_${prepend}" . $_[1];
+	my $newStream = $_[0];
+	my $basis = $_[1];
 	AccuRev("mkstream -s $newStream -b $basis", "Create stream $newStream under $basis.");
 }
 
 sub MakeSnapshot {
-	my $newStream = "${depot}_${prepend}" . $_[0];
-	my $basis = "${depot}_${prepend}" . $_[1];
+	my $newStream = $_[0];
+	my $basis = $_[1];
 	AccuRev("mksnap -s $newStream -b $basis -t now", "Create stream $newStream under $basis.");
 }
 
 sub MakeWorkspace {
-	my $wsName = "${depot}_${prepend}" . $_[0];
-	my $basis = "${depot}_${prepend}" . $_[1];
+	my $wsName = $_[0];
+	my $basis = $_[1];
 	my $location = $_[2];
 	AccuRev("mkws -w $wsName -b $basis -l $location", "Create workspace $wsName under $basis at $location.");
 	chdir $location or die $!;
@@ -256,8 +264,8 @@ sub MakeWorkspace {
 }
 
 sub ReparentWorkspace {
-	my $wsName = "${depot}_${prepend}" . $_[0];
-	my $basis = "${depot}_${prepend}" . $_[1];
+	my $wsName = $_[0];
+	my $basis = . $_[1];
 	AccuRev("chws -w $wsName -b $basis");
 	AccuRev('update');
 }
