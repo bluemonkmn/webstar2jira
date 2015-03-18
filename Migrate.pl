4097cd12e4259734ba693318cb34427203cd9341
Ben Marty <ben.marty@infor.com>
2015-03-18 17:33:22 -0500

    Initial pass of updating migrate script for DMS AccuRev migration.

--- Migrate.pl
+++ Migrate.pl
@@ -5,22 +5,65 @@ my $accurev = 'c:\Program Files (x86)\AccuRev\bin\accurev.exe';
 my $sscmd = 'c:\Program Files (x86)\Microsoft Visual SourceSafe\ss.exe';
 my $tmp = $ENV{'TMP'};
 my $depot = 'FSX';
-my $prepend = 'VisiBar'; # prepended to created stream names in case multiple test imports need to be done.
+my $prepend = 'DMS'; # prepended to created stream names in case multiple test imports need to be done.
 my $rootStream = $prepend ? "${depot}_${prepend}" : $depot;
-my $ssdb = 'C:\Users\bmarty\Downloads\VisiBar';
+my $ssdb = 'C:\Users\bmarty\Downloads\DMS_VSS';
 my $wsDir = "C:\\Users\\bmarty\\AccuRev\\${depot}_${prepend}Migrate";
-my $ssVisiC = '$/VisiBar55';
+my $ssDMSOld = '$/DMS';
+my $ssDMS2 = '$/DMS_FS75E';
 
 LogMsg('Starting migration at ' . localtime());
 
 $ENV{'SSDIR'}=$ssdb;
 MakeStream($rootStream, $depot);
-MakeWorkspace(StmName('Migrate'), $rootStream, $wsDir);
+# DMS Old
+MakeStream(StmName("Old"), $rootStream);
+MakeWorkspace(StmName('Migrate'), StmName("Old"), $wsDir);
 chdir $wsDir or die $!;
-VSSWorkFold($ssVisiC, $wsDir);
+VSSWorkFold($ssDMSOld, $wsDir);
 RecursiveDelete();
-VSSGetByDate($ssVisiC, '12-16-2011');
-CommitAll('Import initial state of VisiBar 5.5C source tree when it still looked like 5.5B on 12-16-2011.');
+VSSGetByDate($ssDMSOld, '8/15/2013;8:22p');
+CommitAll('Import DMS 3.0.1 code as of 8/15/2013 8:22 pm');
+MakeSnapshot(StmName("3.0.1_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '9/11/2013;7:22p');
+CommitAll('Import DMS 3.0.2 code as of 9/11/2013 7:22 pm');
+MakeSnapshot(StmName("3.0.2_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '9/12/2013;7:51p');
+CommitAll('Import DMS 3.0.3 code as of 9/12/2013 7:51 pm');
+MakeSnapshot(StmName("3.0.3_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '9/13/2013;3:54a');
+CommitAll('Import DMS 3.0.4 code as of 9/13/2013 3:54 am');
+MakeSnapshot(StmName("3.0.4_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '10/30/2013;8:19p');
+CommitAll('Import DMS 3.0.5 code as of 10/30/2013 8:19 pm');
+MakeSnapshot(StmName("3.0.5_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '11/07/2013;6:58p');
+CommitAll('Import DMS 3.0.6 code as of 11/07/2013 6:58 pm');
+MakeSnapshot(StmName("3.0.6_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '11/17/2013;7:43p');
+CommitAll('Import DMS 3.0.7 code as of 11/17/2013 7:43 pm');
+MakeSnapshot(StmName("3.0.7_SP"), StmName("Old"));
+RecursiveDelete();
+VSSGetByDate($ssDMSOld, '6/30/2014;11:18p');
+CommitAll('Import DMS 3.0.8 code as of 6/30/2014 11:18 pm');
+MakeSnapshot(StmName("3.0.8_SP"), StmName("Old"));
+
+# DMS 2
+MakeStream(StmName("2"), StmName("Old"));
+print STDERR "Make sure $wsDir is clean, then press enter.\n";
+<STDIN>;
+ReparentWorkspace(StmName('Migrate'), StmName("2"));
+chdir $wsDir or die $!;
+VSSWorkFold($ssDMS2, $wsDir);
+RecursiveDelete();
+VSSGetByDate($ssDMS2 '6/30/2014;11:18p');
+CommitAll('Import DMS 2.0 code as of 6/30/2014 11:18 pm');
 
 sub StmName {
 	return "${depot}_${prepend}_" . $_[0];
